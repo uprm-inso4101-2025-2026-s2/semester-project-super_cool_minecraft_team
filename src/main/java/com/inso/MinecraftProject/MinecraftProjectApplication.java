f1184dae9f64fa12c27adba403b35512444fa390
harryto1 <harry.ruiz6@upr.edu>
2026-02-07 16:22:07 -0400

    initialize Minecraft project with Gradle setup and basic application structure

---
+++ src/main/java/com/inso/MinecraftProject/MinecraftProjectApplication.java
@@ -0,0 +1,13 @@
+package com.inso.MinecraftProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinecraftProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinecraftProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inso/MinecraftProject/MinecraftProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inso.MinecraftProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinecraftProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
