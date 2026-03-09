d4a081ace9946ad98375d6f9f93c41d6207b1cdc
VazquezManuel1 <157653588+VazquezManuel1@users.noreply.github.com>
2026-03-09 10:48:33 -0400

    Add unit tests for /r allowlist redirect

---
+++ src/test/java/com/inso/MinecraftProject/RedirectControllerTest.java
@@ -0,0 +1,41 @@
+package com.inso.MinecraftProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(RedirectController.class)
+class RedirectControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void redirectsAllowedDomain() throws Exception {
+        mvc.perform(get("/r").param("u", "https://modrinth.com/mod/sodium"))
+                .andExpect(status().isFound())
+                .andExpect(header().string("Location", "https://modrinth.com/mod/sodium"));
+    }
+
+    @Test
+    void blocksDisallowedDomain() throws Exception {
+        mvc.perform(get("/r").param("u", "https://evil.com/phish"))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void rejectsMalformedUrl() throws Exception {
+        mvc.perform(get("/r").param("u", "http://"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void rejectsEmptyParam() throws Exception {
+        mvc.perform(get("/r"))
+                .andExpect(status().isBadRequest());
+    }
+}
