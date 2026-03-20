5042ae69915a490317fd93fafeb830c208bd650d
williamcolon58 <william.colon15@upr.edu>
2026-03-20 15:56:06 -0400

    Add upload controller for backend, and upload instructions for frontend

--- src/main/java/com/inso/MinecraftProject/MainController.java
+++ src/main/java/com/inso/MinecraftProject/MainController.java
@@ -7,6 +7,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/upload")
+    public String upload() {
+        return "upload";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+
     @GetMapping("/missing-dependencies")
     public String missingDependencies(Model model) {
         model.addAttribute("loader", "Fabric");
