a90997bf756c2d2e2323e290d7bcbddf7b3dac48
Julian_Laracuente <julian.laracuente@upr.edu>
2026-05-23 15:26:27 -0400

    comment a function so the code compile

--- src/main/java/com/inso/MinecraftProject/service/DependencyLookupCache.java
+++ src/main/java/com/inso/MinecraftProject/service/DependencyLookupCache.java
@@ -83,6 +83,11 @@ public class DependencyLookupCache<T> {
         return cache.size();
     }
 
+    // public void evictExpired(){
+    //     long now = System.currentTimeMillis();
+    //     cache.entrySet().removeIf(entry -> now > entry.getValue().expiresAt);
+    // }
+
     private static class CacheEntry<T> {
         T value;
         long expiresAt;
